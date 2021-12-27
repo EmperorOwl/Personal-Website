9f2a930271b1626758d7f81df0971ff2dbe44a65
EmperorOwl <raylin9703@gmail.com>
2021-12-27 16:17:19 +1100

    Add navigation bar

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ window.onload = function () {
         1500 // 1500 ms = 1.5 s
     );
 };
+
+function showDropdown() {
+
+    "use strict";
+    document.getElementById("dropdown").classList.add("show");
+    document.getElementById("searchBar").classList.add("active");
+}
+
+function hideDropdown() {
+
+    "use strict";
+    document.getElementById("dropdown").classList.remove("show");
+    document.getElementById("searchBar").classList.remove("active");
+}
+
+function filterSearchItems() {
+
+    "use strict";
+    var searchBar = document.getElementById("searchBar"),
+        dropdown = document.getElementById("dropdown"),
+        userSearch = searchBar.value.toLowerCase(),
+        searchItems = dropdown.getElementsByTagName('a'),
+        i = 0;
+
+    for (i = 0; i < searchItems.length; i += 1) {
+        if (searchItems[i].textContent.toLowerCase().includes(userSearch)) {
+            searchItems[i].style.display = "";
+        } else {
+            searchItems[i].style.display = "none";
+        }
+    }
+}
